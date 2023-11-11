58a7797a6042b73f375f175edf02f2c48e5115d9
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-11 13:05:26 -0800

    Add TicketList + Ticket components

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,12 @@
 import React from "react";
 import Header from "./Header";
-
+import TicketList from "./TicketList";
 
 function App(){
-  return (
+  return ( 
     <React.Fragment>
       <Header />
-      <h1>Help Queue</h1>
-      <h3>3a</h3>
-      <h3>{name} and {name2}</h3>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <TicketList />
     </React.Fragment>
   );
 }
