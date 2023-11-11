b303addd267709785ce6e2a7d19ffd0ebbe8bdc8
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-11 13:03:56 -0800

    Add Header component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Header from "./Header";
+
 
 function App(){
   return (
     <React.Fragment>
+      <Header />
       <h1>Help Queue</h1>
       <h3>3a</h3>
-      <h3>Thato and Haley</h3>
+      <h3>{name} and {name2}</h3>
       <p><em>Firebase entries not saving!</em></p>
       <hr/>
     </React.Fragment>
