3105cc4f4daeef710b199f399cde355c68537a28
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-26 11:22:22 -0800

    Use legacy_createStore + add subscribe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { configureStore } from 'redux';
+import { legacy_createStore } from 'redux';
 import reducer from './reducers/ticket-list-reducer';
 import { Provider } from 'react-redux';
 
-const store = configureStore(reducer);
+const store = legacy_createStore(reducer);
+
+store.subscribe(() =>
+  console.log(store.getState())
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
