7d9cdb54a197e62012be592a861bc634ca058db4
Jeremy Josol <jeremyjosol@gmail.com>
2023-11-28 10:15:40 -0800

    Add import + properties

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { v4 } from 'uuid';
 import ReusableForm from "./ReusableForm";
+import { formatDistanceToNow } from 'date-fns';
 
 
 function NewTicketForm(props){
@@ -9,10 +10,14 @@ function NewTicketForm(props){
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
     props.onNewTicketCreation({
-      names: event.target.names.value, 
+      names: event.target.names.value,
       location: event.target.location.value, 
-      issue: event.target.issue.value, 
-      id: v4()
+      issue: event.target.issue.value,
+      id: v4(),
+      timeOpen: new Date(),
+      formattedWaitTime: formatDistanceToNow(new Date(), {
+        addSuffix: true
+      })
     });
   }
 
